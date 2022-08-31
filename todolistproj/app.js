5b2cabaf2e11752e81a02ed0730c00d4230c9184
Luke <87561450+OperationNorthwoods@users.noreply.github.com>
2022-08-31 16:02:38 -0400

    finished todolist with no errors
    had to clean up isNaN. Needs to be called as "Number.isNaN", not on its own as isNan.

--- todolistproj/app.js
+++ todolistproj/app.js
@@ -1,6 +1,5 @@
-const todoList = []
+const todoList = ['go to store', 'return library books', 'learn programming']
 let userInput = '0'
-let deleteInput = '0'
 
 while (userInput !== 'quit') {
     userInput = prompt('Enter your command. Valid commands are "new", "list", "delete" and "quit".').toLowerCase()
@@ -12,7 +11,8 @@ while (userInput !== 'quit') {
         if (todoList.length !== 0) {
             console.log('**************')
             for (i = 0; i < todoList.length; i++) {
-                console.log(`${i} ${todoList[i]}`)}
+                console.log(`${i} ${todoList[i]}`)
+            }
             console.log('**************')
         } else {
             console.log('Your list is empty. Use "new" command to add item')
@@ -20,18 +20,21 @@ while (userInput !== 'quit') {
 
     }
     else if (userInput === 'delete') {
-        if (todoList.length <= 0) {console.log('nothing to delete!')
+        if (todoList.length <= 0) {
+            console.log('nothing to delete!')
         } else {
             console.log('**************')
             for (i = 0; i < todoList.length; i++) {
-                console.log(`${i} ${todoList[i]}`)}
+                console.log(`${i} ${todoList[i]}`)
+            }
             console.log('**************')
-            deleteInput = prompt('enter indice of item to delete from list. Please only enter 1.')
-                if (isNaN(deleteInput = true)){
-                    console.log('Please enter a valid Number!')
-                } else {
-                    todoList.splice(deleteInput,1)
-                }
+            const deleteInput = parseInt(prompt('enter indice of item to delete from list'));
+            if (Number.isNaN(deleteInput)) {
+                console.log('Please enter a valid Number!')
+            } else {
+                todoList.splice(deleteInput, 1)
+                console.log(`indice ${deleteInput} deleted`)
+            }
         }
     } else {
         console.log('try again. press refresh.')
