530f74f18244dfd286786d0764a6e0fd4b217f7a
Luke <87561450+OperationNorthwoods@users.noreply.github.com>
2022-09-22 16:31:34 -0400

    trying to make buttons do stuff
    made a matching codepen too

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+// window.onclick = myFunction;
+
+// function myFunction() {
+//     for (let i = 0; i < 10; i++) {
+//         document.getElementsByid(`a${i}`)[0].classList.add(`itty${i}`);
+//         document.getElementsByid(`a${i}`)[0].style.backgroundColor = "yellow";
+//     }
+// }
+
+myFunction1(){
+    getElementById('demo').innerHTML = " Lorem ipsum dolor sit amet consectetur adipisicing elit.At illum nulla dolor eaque molestias, ut accusantium nihil repellat, tempore odio quia adipisci commodi nobisvelit voluptates cupiditate labore minima ? Nulla."
+}
