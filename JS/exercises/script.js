7dd30c75436a4c40b1a5d2ef22dafa4dc84e472b
Luke <87561450+OperationNorthwoods@users.noreply.github.com>
2022-11-08 16:58:56 -0500

    doing javascript exercises

---
+++ JS/exercises/script.js
@@ -0,0 +1,20 @@
+// https://www.w3resource.com/javascript-exercises/javascript-functions-exercises.php
+//exercise 1: complete
+
+function bob(input) {
+    const letters = []
+    for (x of input) {
+        letters.push(x)
+    }
+    return letters.reverse().join('')
+}
+
+//exercise 2
+
+function isPalindrome(input) {
+    if (input === input.split('').reverse().join('')) {
+        return console.log('true')
+    } else {
+        return console.log('false')
+    }
+}
