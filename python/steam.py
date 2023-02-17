e844ae63cb0fef80dd6d5810e65e8cefda480e0f
Luke <87561450+OperationNorthwoods@users.noreply.github.com>
2023-02-17 16:47:43 -0500

    Still setting up script
    being extra careful, need to finish

--- python/steam.py
+++ python/steam.py
@@ -1,9 +1,9 @@
 import requests
 import json
-from config import API_KEY, STEAM_ID, EMAIL
-import shelve
+from config import API_KEY, STEAM_ID, EMAIL, FILENAME
 from datetime import datetime
 import openpyxl
+from openpyxl.styles.borders import Border, Side
 import ezgmail
 
 current_datetime = datetime.now()
@@ -11,8 +11,19 @@ current_date = current_datetime.strftime('%Y-%m-%d')
 current_time = current_datetime.strftime('%H:%M:%S')
 url = f'https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key={API_KEY}&steamid={STEAM_ID}&format=json '
 response = requests.get(url)
+# Setup the Excel sheet
+wb = openpyxl.load_workbook(f'{FILENAME}')
+sheet = wb.active
+max = sheet.max_row
+# All Columns should have an equal max_row
+#
+# {FILENAME} Column Cheatsheet
+# A=Date B=Time C=Game D=TotalHours
+# E=HoursAddedSinceLastRan F=Last2WeeksHours
+
 
 if response.status_code == 200:
+    
     # Parse the JSON data
     data = json.loads(response.content)
 
@@ -20,10 +31,11 @@ if response.status_code == 200:
         name = game['name']
         playtime_forever = game['playtime_forever']
         playtime_2weeks = game['playtime_2weeks']
-
-        # hours = round((playtime / 60), 2)
-        # print(name, hours)
+    # Start writing to workbook
+    for i in ('A', 'B', 'C', 'D', 'E', 'F'):
+        sheet
     
+
     print('Sucess. Data Fetched from API and written to workbook.')
     ezgmail.send(f'{EMAIL}', 'Sucess!', 'Data Fetched from API and written to workbook.')
 else:
