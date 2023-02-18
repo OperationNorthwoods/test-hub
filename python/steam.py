fe9012c15e13831fb5ee512d3f4622e1a7b8a8ac
Luke <87561450+OperationNorthwoods@users.noreply.github.com>
2023-02-18 16:02:49 -0500

    getting really close
    realizing the "E" column will be hugely complicated and as its maybe the most important piece of  data, I may have to figure out a better way to store it

--- python/steam.py
+++ python/steam.py
@@ -6,9 +6,11 @@ import openpyxl
 from openpyxl.styles.borders import Border, Side
 import ezgmail
 
+# setting up seperate date and time variables
 current_datetime = datetime.now()
 current_date = current_datetime.strftime('%Y-%m-%d')
 current_time = current_datetime.strftime('%H:%M:%S')
+# api call url
 url = f'https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key={API_KEY}&steamid={STEAM_ID}&format=json '
 response = requests.get(url)
 # Setup the Excel sheet
@@ -21,19 +23,42 @@ max = sheet.max_row
 # A=Date B=Time C=Game D=TotalHours
 # E=HoursAddedSinceLastRan F=Last2WeeksHours
 
-
-if response.status_code == 200:
+if response.status_code == 200: # 200 = it worked
     
     # Parse the JSON data
     data = json.loads(response.content)
 
+    gameCounter = 0 # counts number of games in json response
+    gameDict = {}
+
     for game in data['response']['games']:
+        gameCounter+=1
         name = game['name']
         playtime_forever = game['playtime_forever']
         playtime_2weeks = game['playtime_2weeks']
+        gameDict[name] = (playtime_forever, playtime_2weeks)
+        
     # Start writing to workbook
-    for i in ('A', 'B', 'C', 'D', 'E', 'F'):
-        sheet
+    max_Iter = max + 1  # make sure we don't go too far down rows
+    col_A, col_B, col_C, col_D, col_E, col_F = range(1, 7)
+    temp = list(gameDict.keys()) # makes 
+    tempCount = 0
+
+    # main writing loop. loops throught all columns, one row at a time.
+    while max_Iter <= max+gameCounter:
+        sheet.cell(row=max_Iter, column=col_A, value=current_date)
+        sheet.cell(row=max_Iter, column=col_B, value=current_time)
+        sheet.cell(row=max_Iter, column=col_C, value=temp[tempCount])
+        sheet.cell(row=max_Iter, column=col_D, value=gameDict[temp[tempCount]][0])
+        sheet.cell(row=max_Iter, column=col_F, value=gameDict[tempCount][1])
+
+        if max_Iter <= max+gameCounter:
+            print('put bottom border stuff here')
+
+        tempCount+=1
+        max_Iter+=1
+            
+
     
 
     print('Sucess. Data Fetched from API and written to workbook.')
